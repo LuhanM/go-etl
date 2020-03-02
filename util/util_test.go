e44e2d48433c6a6d5fd1f33013d98071e1e8321c
LuhanM <meireles.luhan@gmail.com>
2020-03-01 21:44:56 -0300

    Inclusão de alguns testes unitários do package util

---
+++ util/util_test.go
@@ -0,0 +1,20 @@
+package util
+
+import "testing"
+
+const erro = "valor esperado %v, valor retornado %v"
+
+func TestStringWithoutSpecialCharacter(t *testing.T) {
+	valorEsperado := "12345"
+	resultado := StringWithoutSpecialCharacter([]byte("1.2,3-4/5"))
+	if resultado != valorEsperado {
+		t.Errorf(erro, valorEsperado, resultado)
+	}
+}
+
+func TestGetNullString(t *testing.T) {
+	resultado := GetNullString("NULL")
+	if resultado.Valid {
+		t.Errorf(erro, false, true)
+	}
+}
