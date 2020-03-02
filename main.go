af18dde762af4ec9613a5d0cd6784f421f05b326
LuhanM <meireles.luhan@gmail.com>
2020-03-01 21:23:22 -0300

    Remoção de pacotes comentados

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	// "bufio"
-	// "bytes"
-	// "database/sql"
-	// "fmt"
-	// "io"
 	"log"
 	"net/http"
-	// "strconv"
-	// "time"
 
 	"github.com/LuhanM/go-etl/importador"
 	"github.com/gorilla/mux"
-	//"github.com/lib/pq"
 )
 
 func main() {
